22d4adb88f83beea7dec5dbd0c6c3116c59a3f50
SahilKumar221998 <sahilja265@gmail.com>
2024-05-21 18:01:28 +0530

    Interface having add,edit,delete abstract methods

---
+++ AddressBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+   interface AddressBook
+    {
+        void addPerson();
+        void editPerson();
+        void deletePerson();
+
+    }
+}
